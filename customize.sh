4b67b481ba6e7f1207ab8dafb59a9b9d358869ad
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2020-11-12 19:53:01 +1100

    Uploaded FOSSAPPS

---
+++ customize.sh
@@ -0,0 +1,9 @@
+
+
+#Print welcome message
+ui_print "*******************************"
+ui_print "           FOSSAPPS          "
+ui_print "*******************************"
+ui_print "- Installing FOSSAPPS, Free and Open source apps"
+ui_print "- By Wacko, reddit: wacko1805"
+ui_print "- Visit my site for more: un.pixel-fy.com"
